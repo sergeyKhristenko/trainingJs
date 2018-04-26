94ef6a50a40c09ea6fd40a7b243fa47934fd2735
Sergey Khristenko <sergey.khristenko@gmail.com>
2018-04-26 15:09:50 +0300

    fixed L1T2 task

--- L1T2/script.js
+++ L1T2/script.js
@@ -1,12 +1,25 @@
 const number = 6;
 
 const isPrime = num => {
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    return !!(num % i)
+  if(num === 1 || !Number.isInteger(num)) {
+    return false;
   }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+
+  return true
 };
 
-console.assert(isPrime(5) === true);
-console.assert(isPrime(6) === false);
+console.assert(isPrime(5) === true, '5');
+console.assert(isPrime(6) === false, '6');
+console.assert(isPrime(2) === true, '2');
+console.assert(isPrime(3) === true, '3');
+console.assert(isPrime(1) === false, '1');
+console.assert(isPrime(2.2) === false, '2.2');
+console.assert(isPrime(2.22) === false, '2.2');
 
-console.log(isPrime(number));
+console.log(isPrime(5));
